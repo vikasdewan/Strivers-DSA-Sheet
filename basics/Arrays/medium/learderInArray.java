dd99d2340332b117998afff187e472c3325499ac
VikasDewangan <dewanganvikas192@gmail.com>
2025-05-29 11:27:03 +0530

    better approach

--- basics/Arrays/medium/learderInArray.java
+++ basics/Arrays/medium/learderInArray.java
@@ -9,6 +9,10 @@ public class learderInArray {
         int[] arr = {-3,4,5,1,-4,-5};
          ArrayList<Integer> ans = leaders(arr);
          System.out.println(ans);
+         ArrayList<Integer> ans2 = leadersBetter(arr);
+         System.out.println(ans2);
+         
+
     }
 
 
@@ -28,5 +32,33 @@ public class learderInArray {
         }
         list.add(nums[nums.length-1]);
         return list;    
+    }
+
+
+    //better approach
+    public static ArrayList<Integer> leadersBetter(int[] arr){
+        int max = Integer.MIN_VALUE;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = arr.length-1;i>=0;i--){
+           
+           if(max<arr[i]){
+                max=arr[i];
+                list.add(arr[i]);
+            }
+        }
+        
+        return reverse(list);
+
+         
+    }
+
+    public static ArrayList<Integer> reverse(ArrayList<Integer> list){
+         
+        ArrayList<Integer> newList = new ArrayList<>();
+
+        for(int i = list.size()-1; i>=0;i--){
+            newList.add(list.get(i));
+        }
+        return newList;
     }
 }
