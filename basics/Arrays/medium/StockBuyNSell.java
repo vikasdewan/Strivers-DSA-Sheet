d0c97699b7bd9fe463be2562443ef29d2097ba33
VikasDewangan <dewanganvikas192@gmail.com>
2025-06-01 00:13:43 +0530

    half century complete | 50 day streak

--- basics/Arrays/medium/StockBuyNSell.java
+++ basics/Arrays/medium/StockBuyNSell.java
@@ -1,32 +1,33 @@
 // https://takeuforward.org/plus/dsa/problems/best-time-to-buy-and-sell-stock
 package Arrays.medium;
+
 import java.util.*;
 
-public class StockBuyNSell{
+public class StockBuyNSell {
 
     public static void main(String[] args) {
-        
-        int[] arr = {7,1,5,3,6,4};
+
+        int[] arr = { 7, 1, 5, 3, 6, 4 };
         int profit = profit(arr, arr.length);
         System.out.println(profit);
 
     }
 
-    public static int profit(int[] arr, int n){
+    public static int profit(int[] arr, int n) {
         int maxVal = Integer.MIN_VALUE;
         int minVal = Integer.MAX_VALUE;
         int L = 0;
-        int R = n-1;
-        while(L<=R){
-            if(arr[L] < minVal){
+        int R = n - 1;
+        while (L <= R) {
+            if (arr[L] < minVal) {
                 minVal = arr[L];
             }
-            if(arr[R] > maxVal){
+            if (arr[R] > maxVal) {
                 maxVal = arr[R];
             }
             L++;
             R--;
         }
-        return maxVal-minVal;
+        return maxVal - minVal;
     }
 }
