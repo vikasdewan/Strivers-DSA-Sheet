e328b8522647f98d9c0bb00ac21bc1475eeeec70
VikasDewangan <dewanganvikas192@gmail.com>
2025-06-07 18:05:23 +0530

    binary search to find an element on sorted array.

---
+++ basics/BinarySearchQuestions/FindXSortedArray.java
@@ -0,0 +1,45 @@
+package BinarySearchQuestions;
+
+public class FindXSortedArray {
+    
+    public static void main(String[] args) {
+        
+        int[] arr = {-1,0,3,5,9,12};
+        int target = 9;
+        System.out.println(indexOfTarget(arr, target));
+        System.out.println(findIdxRecursive(arr, 0, arr.length-1, target));
+    }
+
+    public static int indexOfTarget(int[] arr,int target){
+        int s = 0;
+        int e = arr.length-1;
+        while(s<=e){
+            int m = (s + e) / 2;
+            if(arr[m] == target){
+                return m;
+            }else if(target < arr[m]){
+                e = m-1;
+            }else{
+                s=m+1;
+            }
+        }
+        
+        return -1;
+    }
+
+    //recursive approach
+     public static int findIdxRecursive(int[] arr,int s ,int e , int target){
+        if(s>e){
+            return -1;
+        }
+
+       int m = s + (e-s)/2;
+        if(arr[m] == target){
+            return m;
+        }else if(target < arr[m]){
+            return findIdxRecursive(arr, s, m-1, target);
+        }else{
+            return findIdxRecursive(arr, m + 1, e, target);
+        }
+     }
+}
