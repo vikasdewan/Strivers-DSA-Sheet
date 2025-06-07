81d6982326f8dcc31aaa048f047f4d1929d3850e
VikasDewangan <dewanganvikas192@gmail.com>
2025-06-07 18:25:12 +0530

    lowerBoound  , understand.

---
+++ basics/BinarySearchQuestions/LowerBound.java
@@ -0,0 +1,30 @@
+package BinarySearchQuestions;
+
+public class LowerBound {
+
+    public static void main(String[] args) {
+
+        int[] arr = { 2, 7, 9, 10, 13 };
+        int x = 15;
+        System.out.println(lowerBoundForX(arr, x));
+    }
+
+    public static int lowerBoundForX(int[] arr, int x) {
+
+        int lowerBound = arr.length;
+        int s = 0;
+        int e = arr.length - 1;
+        while (s <= e) {
+            int m = (s + e) / 2;
+            if (arr[m] >= x) {
+                lowerBound = m;
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
+
+        }
+        return lowerBound;
+    }
+
+}
