67cdfe4c2ae8865a76350f04817553654112347c
VikasDewangan <dewanganvikas192@gmail.com>
2025-06-11 11:33:52 +0530

    `Added searchInARotatedSortedArrayII method to SearchRotatedSortedArray class with functionality to search for an element in a rotated sorted array with duplicate entries.`

--- basics/BinarySearchQuestions/SearchRotatedSortedArray.java
+++ basics/BinarySearchQuestions/SearchRotatedSortedArray.java
@@ -4,9 +4,10 @@ public class SearchRotatedSortedArray {
     
     public static void main(String[] args) {
         
-        int[] arr = {8,10,12,2,4,6};
-        int target = 4;
+        int[] arr = {3,1,2,3,3,3,3};
+        int target = 3;
         System.out.println(search(arr, target));
+        System.out.println(searchInARotatedSortedArrayII(arr, target));
     }
 
 
@@ -60,4 +61,46 @@ public class SearchRotatedSortedArray {
         }
         return -1;
     }
+
+
+        //optimal 2 //for duplicate entries.
+        public static boolean searchInARotatedSortedArrayII(int []arr, int k) {
+        int n = arr.length; // size of the array.
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            //if mid points the target
+            if (arr[mid] == k) return true;
+
+            //Edge case:
+            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low = low + 1;
+                high = high - 1;
+                continue;
+            }
+
+            //if left part is sorted:
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= k && k <= arr[mid]) {
+                    //element exists:
+                    high = mid - 1;
+                } else {
+                    //element does not exist:
+                    low = mid + 1;
+                }
+            } else { //if right part is sorted:
+                if (arr[mid] <= k && k <= arr[high]) {
+                    //element exists:
+                    low = mid + 1;
+                } else {
+                    //element does not exist:
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
+
 }
