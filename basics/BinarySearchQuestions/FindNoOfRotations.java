66460198962d9758970370d21cb60ce5fb9c36ae
VikasDewangan <dewanganvikas192@gmail.com>
2025-06-12 08:13:51 +0530

    number of rotations

---
+++ basics/BinarySearchQuestions/FindNoOfRotations.java
@@ -0,0 +1,24 @@
+package BinarySearchQuestions;
+
+public class FindNoOfRotations {
+    
+    public static void main(String[] args) {
+        
+        int[] arr = {4,5,6,7,0,1,2,3};
+        System.out.println(findRotation(arr));
+    }
+
+    public static int findRotation(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while(start < end){
+            int mid = start + (end - start)/2;
+            if(arr[mid]> arr[mid+1]){
+                end = mid;
+            }else{
+                start = mid+1;
+            }
+        }
+        return (end+1)%arr.length;
+    }
+}
