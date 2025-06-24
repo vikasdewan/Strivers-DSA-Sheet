5225fd43ce7dd23b14711b9beec564488859de0c
VikasDewangan <dewanganvikas192@gmail.com>
2025-06-24 06:25:50 +0530

    optimal solution

--- basics/BinarySearchQuestions/Lec3/Max1sInRow.java
+++ basics/BinarySearchQuestions/Lec3/Max1sInRow.java
@@ -34,4 +34,38 @@ public class Max1sInRow {
         return maxIdx;
     }
 
+
+        //optimal solution
+        public static int lowerBound(ArrayList<Integer> arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr.get(mid) >= x) {
+                ans = mid;
+                // look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+    public static int rowWithMax1s(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
+        int cnt_max = 0;
+        int index = -1;
+
+        // traverse the rows:
+        for (int i = 0; i < n; i++) {
+            // get the number of 1's:
+            int cnt_ones = m - lowerBound(matrix.get(i), m, 1);
+            if (cnt_ones > cnt_max) {
+                cnt_max = cnt_ones;
+                index = i;
+            }
+        }
+        return index;
+    }
 }
